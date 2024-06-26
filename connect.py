c95f03e0ca64b085fa5ccfbcda6e2585257464e6
Dharshini050417 <dharshiniga.cse2022@citchennai.com>
2024-06-26 10:38:17 +0530

    bhumi-ml initiated

---
+++ connect.py
@@ -0,0 +1,17 @@
+from config import load_config
+import psycopg2
+
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
+if __name__ == '_main_':
+    config = load_config()
+    connect(config)
